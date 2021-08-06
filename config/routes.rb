b91757be4f351fac532b1c885bea60e4485e56d6
Jose Torres <pepe_torres1998@hotmail.com>
2021-08-06 02:52:44 -0500

    Customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'sign_up', sign_up: '' }
   root 'static_pages#home'
 
   resources :wallets
